e6087b08217e5631f2997226650437211a4d22dd
MD. ARIFUL ISLAM <tonu2050@gmail.com>
2022-06-12 00:28:18 +0600

    comm

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,12 +1,12 @@
 //Development Mode Start//
 // export const JPA_API_URL = "http://localhost";
 
-// export const JPA_API_URL = "http://localhost:8081";
+export const JPA_API_URL = "http://localhost:8082";
 
 // export const JPA_API_URL = "http://10.11.201.27:8081";
 
 //AWS EC2 Server
-export const JPA_API_URL = "http://34.228.16.241:9002";
+// export const JPA_API_URL = "http://34.228.16.241:9002";
 
 //Production Mode Start//
 // export const JPA_API_URL = "http://192.183.155.121:9001";
